9743a438c3b1e8ac3f39f9f5b85b5982aefdc57c
mryhryki <mryhryki@gmail.com>
2023-02-21 17:52:09 +0900

    Add annotations to health endopoint

--- src/health.controller.ts
+++ src/health.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('health')
 export class HealthController {
   @Get()
+  @Header("Content-Type", "application/json; charset=utf-8")
   getHealth(): string {
     const timestamp = new Date().toISOString();
     return JSON.stringify({ timestamp }, null, 2);
